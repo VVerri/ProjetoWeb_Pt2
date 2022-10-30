3cb3b70c8aacd8ef315b897bcd80f86564e73db8
Victor Verri <105087775+VVerri@users.noreply.github.com>
2022-10-30 02:34:34 -0300

    Added by Upload
    Login funcionando com Reqres, iniciando Logado e Busca

--- Login/login.js
+++ Login/login.js
@@ -1,23 +1,60 @@
-let button = document.querySelector('button');
-button.addEventListener('click', () =>{
-    let overlay = document.createElement('div');
-    overlay.className = 'overlay';
-    document.body.appendChild(overlay);
+let loginReq = new XMLHttpRequest();
 
-    let dialog = document.createElement('div');
-    dialog.className = 'dialog'
+function login() {
 
-    let span = document.createElement('span');
-    span.innerHTML = 'Dialog supimpa'
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('senha').value
 
-    let button = document.createElement('button');
-    button.innerHTML = 'Fechar'
-    button.addEventListener('click', () => {
-        document.body.removeChild(overlay);
-        document.body.removeChild(dialog);
-    })
+  if (validation(email, password)) {
 
-    dialog.appendChild(span);
-    dialog.appendChild(button);
-    document.body.appendChild(dialog);
-})
+    let data = {
+      email,
+      password
+    }
+    loginReq.open("POST", "https://reqres.in/api/login", true);
+    loginReq.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+    loginReq.onreadystatechange = function () {
+      if (loginReq.readyState === 4 && loginReq.status === 200) {
+        localStorage.setItem("token", JSON.parse(loginReq.responseText).token)
+        document.getElementsByClassName('loginBtn')[0];
+        document.getElementsByClassName('modalCont')[0].style.display = 'none';
+        document.getElementsByClassName('grid-dir')[0].style.display = 'none';
+        document.getElementsByClassName('logado')[0].style.display = 'flex';
+
+        document.getElementsByClassName('open-account')[0].style.display = 'none'
+        document.getElementsByClassName('soma-btn')[0].style.display = 'none'
+        toggleText();
+        
+
+      } else if (loginReq.status === 400) {
+        document.getElementsByClassName('email-erro')[0].style.display = 'flex'
+        document.getElementsByClassName('email-erro')[0].textContent = '⚠ Login inválido ⚠'
+
+      }
+    }
+    loginReq.send(JSON.stringify(data));
+  }
+
+}
+
+function toggleText(buttonLogin)  {
+    var text = document.getElementById(buttonLogin).firstChild;
+    text.data = text.data == "Acesse sua conta" ? "Olá, " + email : "Acesse sya conta";
+ }
+
+function validation(email, password) {
+  if (email.length < 3) {
+    document.getElementsByClassName('email-erro')[0].style.display = 'flex'
+    document.getElementsByClassName('email-erro')[0].textContent = '⚠ E-mail inválido ⚠'
+    return false
+  }
+  if (password.length < 3) {
+    document.getElementsByClassName('senha-erro')[0].style.display = 'flex'
+    document.getElementsByClassName('senha-erro')[0].textContent = '⚠ Senha inválida ⚠'
+    return false
+  }
+  return true
+}
+
+const submit = document.getElementsByClassName('submit-btn')[0];
+submit.addEventListener('click', login);
